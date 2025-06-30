7d0e1f72fd3533bd98c1ee13d2c5176f1ec25395
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-07-01 00:15:16 +0300

    update sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -10,7 +10,19 @@ export default defineConfig({
   projectId: 'qgwhyrdi',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool({
+      structure: (S) =>
+        S.list()
+          .title('Контент')
+          .items([
+            S.listItem()
+              .title('Тури')
+              .child(S.documentList().title('Тури').filter('_type == "tour"')),
+          ]),
+    }),
+    visionTool(),
+  ],
 
   schema: {
     types: schemaTypes,
